38b7a3adea17b6c8ce46aec93075ba1a4b03d706
Anshul Gupta <anshul.dlf@gmail.com>
2015-06-20 23:08:41 -0700

    error handling code in integrations

--- server/integrations/foursquare.py
+++ server/integrations/foursquare.py
@@ -7,83 +7,86 @@ def find_foursquare_url(lat, lon, name):
 	Given a latitude, longitude and the name of a restaurant or a bar from Google Places API
 	response, returns mobile url for Foursquare
 	"""
-	SEARCH_API_URL = 'https://api.foursquare.com/v2/venues/search'
-	VENUE_API_URL = 'https://api.foursquare.com/v2/venues/'
-	EXPLORE_API_URL = 'https://api.foursquare.com/v2/venues/explore'
+	try:
+		SEARCH_API_URL = 'https://api.foursquare.com/v2/venues/search'
+		VENUE_API_URL = 'https://api.foursquare.com/v2/venues/'
+		EXPLORE_API_URL = 'https://api.foursquare.com/v2/venues/explore'
 
-	params = {
-        'client_id': FOURSQUARE_CLIENT_ID,
-        'client_secret' : FOURSQUARE_CLIENT_SECRET,
-        'll': '{lat},{lon}'.format(lat=lat, lon=lon),
-        'radius': '100',
-        'v': '20141016',
-        'query': name,
-        'limit': '1',
-        'intent': 'match'
-    }
-	response = requests.get(SEARCH_API_URL, params=params)
+		params = {
+	        'client_id': FOURSQUARE_CLIENT_ID,
+	        'client_secret' : FOURSQUARE_CLIENT_SECRET,
+	        'll': '{lat},{lon}'.format(lat=lat, lon=lon),
+	        'radius': '100',
+	        'v': '20141016',
+	        'query': name,
+	        'limit': '1',
+	        'intent': 'match'
+	    }
+		response = requests.get(SEARCH_API_URL, params=params)
 
-	# Request failed
-	if not response.ok:
-		print "[FOURSQUARE] failed: ", response.json()
-		return json_error_message("FOURSQUARE API request failed",
-                                  error_data=response.json())
-	
-	result = response.json()['response']['venues']
-	print result
-	if len(result) == 0:
-		return
+		# Request failed
+		if not response.ok:
+			print "[FOURSQUARE] failed: ", response.json()
+			return json_error_message("FOURSQUARE API request failed",
+	                                  error_data=response.json())
+		
+		result = response.json()['response']['venues']
+		if len(result) == 0:
+			return
 
-	venueId = result[0]['id']
+		venueId = result[0]['id']
 
-	# No results found
-	if len(result) == 0:
-		print "[FOURSQUARE] no results found,"
-		return json_error_message("No places found near this location.")
+		# No results found
+		if len(result) == 0:
+			print "[FOURSQUARE] no results found,"
+			return json_error_message("No places found near this location.")
 
-	venueParams = {
-        'client_id': FOURSQUARE_CLIENT_ID,
-        'client_secret' : FOURSQUARE_CLIENT_SECRET,
-        'v': '20141016'
-    }
-	venueResponse = requests.get(VENUE_API_URL+venueId, params=venueParams).json()['response']['venue']
+		venueParams = {
+	        'client_id': FOURSQUARE_CLIENT_ID,
+	        'client_secret' : FOURSQUARE_CLIENT_SECRET,
+	        'v': '20141016'
+	    }
+		venueResponse = requests.get(VENUE_API_URL+venueId, params=venueParams).json()['response']['venue']
 
 
-	shortUrl = venueResponse['shortUrl']
-	specials = venueResponse['specials']
-	rating = venueResponse.get('rating')
+		shortUrl = venueResponse['shortUrl']
+		specials = venueResponse['specials']
+		rating = venueResponse.get('rating')
 
-	# Find better alternatives
+		# Find better alternatives
 
-	exploreParams = {
-        'client_id': FOURSQUARE_CLIENT_ID,
-        'client_secret' : FOURSQUARE_CLIENT_SECRET,
-        'll': '{lat},{lon}'.format(lat=lat, lon=lon),
-        'radius': '900',
-        'v': '20141016',
-        'limit': '5',
-        'intent': 'nextVenues',
-        'lastVenue': venueId,
-        'section' : 'food'
-    }
-	exploreResponse = requests.get(EXPLORE_API_URL, params=exploreParams).json()['response']['groups'][0]['items']
+		exploreParams = {
+	        'client_id': FOURSQUARE_CLIENT_ID,
+	        'client_secret' : FOURSQUARE_CLIENT_SECRET,
+	        'll': '{lat},{lon}'.format(lat=lat, lon=lon),
+	        'radius': '900',
+	        'v': '20141016',
+	        'limit': '5',
+	        'intent': 'nextVenues',
+	        'lastVenue': venueId,
+	        'section' : 'food'
+	    }
+		exploreResponse = requests.get(EXPLORE_API_URL, params=exploreParams).json()['response']['groups'][0]['items']
 
-	alternatives = []
-	for item in exploreResponse:
-		if(item['venue']['url'] == None):
-			continue	
-		alternatives.append({
-			"name": item['venue'].get('name'),
-        	"specials": item['venue']['specials'],
-        	"rating" : item['venue'].get('rating')})
+		alternatives = []
+		for item in exploreResponse:
+			if(item['venue']['url'] == None):
+				continue	
+			alternatives.append({
+				"name": item['venue'].get('name'),
+	        	"specials": item['venue']['specials'],
+	        	"url" : item['venue'].get("url"),
+	        	"rating" : item['venue'].get('rating')})
 
-	return {
-		"name": venueResponse.get('name'),
-        "4sqUrl": shortUrl,
-        "url": venueResponse.get('url'),
-        "specials": specials,
-        "rating" : rating,
-        "explore": alternatives
-    }
+		return {
+			"name": venueResponse.get('name'),
+	        "4sqUrl": shortUrl,
+	        "url": venueResponse.get('url'),
+	        "specials": specials,
+	        "rating" : rating,
+	        "explore": alternatives
+	    }
+	except:
+		return None
 # Sample response object from Google Places API
 # name: "Shake Shack", lat: 40.77912606570555, lon: -73.95490489457822
--- server/integrations/open_table.py
+++ server/integrations/open_table.py
@@ -6,25 +6,28 @@ def find_open_table_url(restaurant):
 	Given a restaurant from Google Places API response, returns mobile url for
 	OpenTable reservation if it exists, otherwise results None
 	"""
-	name = restaurant.get('name').replace('&', 'and').replace(' ', '+')
-	vicinity = restaurant.get('vicinity')
-	(address, city) = vicinity.split(',')
-	address = address.replace(' ', '+')
-	city = city.lstrip().replace(' ', '+')
+	try:
+		name = restaurant.get('name').replace('&', 'and').replace(' ', '+')
+		vicinity = restaurant.get('vicinity')
+		(address, city) = vicinity.split(',')
+		address = address.replace(' ', '+')
+		city = city.lstrip().replace(' ', '+')
 
-	ot_url = 'http://opentable.herokuapp.com/api/restaurants?'
-	if name:
-		ot_url += ('name=' + name + '&')
-	if vicinity:
-		ot_url += ('address=' + address + '&city=' + city)
+		ot_url = 'http://opentable.herokuapp.com/api/restaurants?'
+		if name:
+			ot_url += ('name=' + name + '&')
+		if vicinity:
+			ot_url += ('address=' + address + '&city=' + city)
 
-	response = requests.get(ot_url)
-	restaurant_json = json.loads(response.text)
+		response = requests.get(ot_url)
+		restaurant_json = json.loads(response.text)
 
-	resto = restaurant_json.get('restaurants')
-	if resto:
-		resto = resto[0]
-	if resto:
-		return resto.get('mobile_reserve_url')
-	else:
+		resto = restaurant_json.get('restaurants')
+		if resto:
+			resto = resto[0]
+		if resto:
+			return resto.get('mobile_reserve_url')
+		else:
+			return None
+	except:
 		return None
